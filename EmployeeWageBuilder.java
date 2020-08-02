242b3e6a468d2bc1153a7377bcf5a73c9ed7a2df
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-02 13:53:23 +0530

    Added : Initail Commit 'Welcome file added'

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+/* 
+  Employee Wage Computation Program.....
+*/
+class EmployeeWageBuilder{
+	public static void main(String[] args){
+		System.out.println("Welcome To Employee Wage Computation Program");
+	}
+}
