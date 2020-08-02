f4e3bbf7774e9813b4bfb15fe56c46d8354f5cec
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-02 17:49:25 +0530

    Refactore : refactore the code for wage calculation.

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -12,6 +12,15 @@ class EmployeeWageBuilder {
 	int total_EmpWage=0;		
 	int total_working_day=0;
 	int total_empHrs = 0;
+	public void wage_Compute(){
+			emp_Wage = Emp_Rate_PerHr * emp_FullDay_Hr ;		
+			System.out.println("Employee's [day-" + total_working_day + "] wage is " + emp_Wage);
+	}
+	public void wage_display(){
+		System.out.println("Totla Working hours " + total_empHrs );
+		System.out.println("Employee Total wage For a Month : " + total_EmpWage);
+	}
+
 	public void emp_Wage_Cal(){
 		while(total_working_day < Total_Work_Day && total_empHrs < Total_Working_Hr_Month ){
 			total_working_day++;
@@ -27,14 +36,12 @@ class EmployeeWageBuilder {
 					emp_FullDay_Hr = 0;
 				break ;
 			}
-			emp_Wage = Emp_Rate_PerHr * emp_FullDay_Hr ;		
 			total_EmpWage += emp_Wage ;
 			total_empHrs += emp_FullDay_Hr;
-			System.out.println("Employee's [day-" + total_working_day + "] wage is " + emp_Wage);
+			wage_Compute();	
 		}	
-		System.out.println("Totla Working hours " + total_empHrs );
-		System.out.println("Employee Total wage For a Month : " + total_EmpWage);
-	}
+			wage_display();
+}
 //main Method 
 	public static void main(String[] args){
 		 EmployeeWageBuilder present_check =  new EmployeeWageBuilder();
