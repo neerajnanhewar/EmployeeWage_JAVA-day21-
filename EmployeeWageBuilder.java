09fd2e5a6ac65d428fc3200c5c82ee28674fe3a3
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-02 17:39:51 +0530

    Added : Working Hours and Month added

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,11 +6,15 @@ class EmployeeWageBuilder {
 	static final int Is_Part_Time = 2;
 	static final int Emp_Rate_PerHr = 20 ;
 	static final int Total_Work_Day = 20;
+	static final int Total_Working_Hr_Month = 100;
 	int emp_FullDay_Hr = 0 ;
 	int emp_Wage = 0 ;
 	int total_EmpWage=0;		
+	int total_working_day=0;
+	int total_empHrs = 0;
 	public void emp_Wage_Cal(){
-		for(int day=1;day<=Total_Work_Day;day++){
+		while(total_working_day < Total_Work_Day && total_empHrs < Total_Working_Hr_Month ){
+			total_working_day++;
 			int empCheck= (int) Math.floor(Math.random()*10) % 3 ;		
 			switch (empCheck){
 				case Is_Full_Time :
@@ -25,8 +29,10 @@ class EmployeeWageBuilder {
 			}
 			emp_Wage = Emp_Rate_PerHr * emp_FullDay_Hr ;		
 			total_EmpWage += emp_Wage ;
-				System.out.println("Employee's [day-" + day + "] wage is " + emp_Wage);
+			total_empHrs += emp_FullDay_Hr;
+			System.out.println("Employee's [day-" + total_working_day + "] wage is " + emp_Wage);
 		}	
+		System.out.println("Totla Working hours " + total_empHrs );
 		System.out.println("Employee Total wage For a Month : " + total_EmpWage);
 	}
 //main Method 
